baecfdc7cf7cf47662b4fc84a017f5d430e300f3
NoteBook <rlaqls16@gmail.com>
2018-12-27 22:26:21 +0900

    파일 열기 기능 추가(Add the Open Files Func)
    파일 열기 기능 추가
    
    Localization
    Pool_FamilyNames,
    Pool_FemaleNames,
    Pool_MaleNames,
    Pool_PetNames,
    PS4Lang,
    TempLocalization,
    XboneLang
    
    위의 파일은 [언어 관련 파일]입니다.
    없을 경우에는 fasle 반환합니다.
    /////////////////////////////////////////////////////
    Add the Open Files Func
    
    Localization
    Pool_FamilyNames,
    Pool_FemaleNames,
    Pool_MaleNames,
    Pool_PetNames,
    PS4Lang,
    TempLocalization,
    XboneLang
    
    Above file list is [Language Files List].
    If files not exist than  return false

--- Sheltered Translation Program/FileLoad.Designer.cs
+++ Sheltered Translation Program/FileLoad.Designer.cs
@@ -35,21 +35,24 @@
             // 
             // btn_translation
             // 
+            this.btn_translation.Enabled = false;
             this.btn_translation.Location = new System.Drawing.Point(12, 56);
             this.btn_translation.Name = "btn_translation";
-            this.btn_translation.Size = new System.Drawing.Size(523, 27);
+            this.btn_translation.Size = new System.Drawing.Size(594, 27);
             this.btn_translation.TabIndex = 0;
-            this.btn_translation.Text = "번역하기";
+            this.btn_translation.Text = "번역하기(Translation)";
             this.btn_translation.UseVisualStyleBackColor = true;
+            this.btn_translation.Click += new System.EventHandler(this.btn_translation_Click);
             // 
             // btn_open
             // 
             this.btn_open.Location = new System.Drawing.Point(364, 21);
             this.btn_open.Name = "btn_open";
-            this.btn_open.Size = new System.Drawing.Size(171, 25);
+            this.btn_open.Size = new System.Drawing.Size(242, 25);
             this.btn_open.TabIndex = 1;
-            this.btn_open.Text = "파일 위치 지정하기";
+            this.btn_open.Text = "파일 위치 지정하기(Open Files)";
             this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
             // 
             // textBox1
             // 
@@ -62,7 +65,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 93);
+            this.ClientSize = new System.Drawing.Size(618, 93);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_open);
             this.Controls.Add(this.btn_translation);
--- Sheltered Translation Program/FileLoad.cs
+++ Sheltered Translation Program/FileLoad.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Sheltered_Translation_Program
 {
@@ -15,6 +16,68 @@ namespace Sheltered_Translation_Program
         public FileLoad()
         {
             InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.Description = "Language 폴더 선택\nSelect Language Folder";
+                fbd.ShowDialog();
+                string path = fbd.SelectedPath;
+                if (CheckedFiles(path) == true)
+                {
+                    btn_translation.Enabled = true;
+                }
+                else MessageBox.Show("파일이 부족합니다.");
+            }
+        }
+
+        private void btn_translation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool CheckedFiles(string Path)
+        {
+            DirectoryInfo di = new DirectoryInfo(Path);
+            FileInfo[] fi = new FileInfo[di.GetFiles().Count()];
+            
+            for (int i = 0; i < 8; i++)
+            {
+                fi = di.GetFiles(FileName(i));
+                if (fi.Count() == 0) return false;
+            }
+            return true;
+        }
+
+        enum FileNames
+        {
+            Localization = 0,
+            Pool_FamilyNames,
+            Pool_FemaleNames,
+            Pool_MaleNames,
+            Pool_PetNames,
+            PS4Lang,
+            TempLocalization,
+            XboneLang = 7
+        }
+
+        private string FileName(int number)
+        {
+            string Name = null;
+            switch (number)
+            {
+                case 0: Name = FileNames.Localization.ToString(); break;
+                case 1: Name = FileNames.Pool_FamilyNames.ToString(); break;
+                case 2: Name = FileNames.Pool_FemaleNames.ToString(); break;
+                case 3: Name = FileNames.Pool_MaleNames.ToString(); break;
+                case 4: Name = FileNames.Pool_PetNames.ToString(); break;
+                case 5: Name = FileNames.PS4Lang.ToString(); break;
+                case 6: Name = FileNames.TempLocalization.ToString(); break;
+                case 7: Name = FileNames.XboneLang.ToString(); break;
+            }
+            return Name + ".txt";
         }
     }
 }
